a3df684844f6848a64609507a2bd9135d148a047
Pavel Ovchinnikov <99139074+R1senDev@users.noreply.github.com>
2022-03-28 21:47:09 +0300

    Update

--- main.js
+++ main.js
@@ -11,6 +11,10 @@ let safezone = 8;
 let enemy
 let backgroundColor = 'black';
 let maxEnemyHp = 50;
+let enemiesLimit = 1;
+let bulletsRadius = 10;
+let bulletsDiameter = bulletsRadius * 2;
+let bulletsColor = 'purple';
 
 class Overlay_ {
 	constructor() {
@@ -66,8 +70,13 @@ function redraw() {
 	ctx.fillStyle = player.color;
 	ctx.fillRect(player.x - camera.x.relative, player.y - camera.y.relative, player.size, player.size);
 	for (let i of enemies) {
-		if (fps.frames == 100) {console.log(i.radius);}
 		circle(i.x + i.radius - camera.x.relative, i.y + i.radius - camera.y.relative, i.radius, i.color);
+	}
+	for (let i of playerBullets) {
+		circle(i.x, i.y, bulletsRadius, bulletsColor);
+	}
+	for (let i of enemyBullets) {
+		circle(i.x, i.y, bulletsRadius, bulletsColor);
 	}
 
 	ctx.fillStyle = 'green';
@@ -86,16 +95,112 @@ let keys = {
 	pressed: [],
 };
 
+class PlayerBullet {
+	constructor(direction) {
+		this.x = player.x;
+		this.y = player.y;
+		this.move = function() {};
+		switch (direction) {
+			case 0:
+				this.move = function() {
+					this.y--;
+				};
+				break;
+			case 1:
+				this.move = function() {
+					this.x++;
+				};
+				break;
+			case 2:
+				this.move = function() {
+					this.y++;
+				};
+				break;
+			case 3:
+				this.move = function() {
+					this.x--;
+				};
+				break;
+		}
+	}
+}
+class Player_ {
+	constructor() {
+		this.size = 50;
+		this.hp = 5;
+		this.x = Math.round(map.width / 2 - this.size / 2);
+		this.y = Math.round(map.height / 2 - this.size / 2);
+		this.dots = [];
+		this.color = 'white';
+		this.shootCooldown = 0;
+	}
+	get pos() {
+		return [this.x, this.y];
+	}
+	updateDots() {
+		this.dots = [];
+		for (let y = this.y; y <= this.y + this.size; y++) {
+			for (let x = this.x; x <= this.x + this.size; x++) {
+				this.dots.push(`${x}:${y}`);
+			}
+		}
+	}
+	move(x, y) {
+		this.x += x;
+		this.y += y;
+		camera.x.absolute += x;
+		camera.y.absolute += y;
+		this.updateDots();
+	}
+	shoot(direction) {
+		if (this.shootCooldown <= 0) {
+			let bulletSpawned = false;
+			for (let i of playerBullets) {
+				if ((i.x == canvas.width) && (i.y == canvas.height)) {
+					i = new PlayerBullet(direction);
+					bulletSpawned = true;
+					break;
+				}
+			}
+			if (!bulletSpawned) {
+				playerBullets.push(new PlayerBullet(direction));
+			}
+			this.shootCooldown = 10;
+		}
+	}
+	harm() {
+		this.hp--;
+		camera.shake(5, 50, 1000);
+		if (this.hp <= 0) {
+			player.kill();
+		}
+	}
+	kill() {
+		this.gameOver = true;
+		camera.deathEffect();
+	}
+	respawn() {
+		respawnPlayer();
+		camera.respawnEffect();
+	}
+}
+let player = new Player_();
+let playerBullets = [];
+function respawnPlayer() {
+	player = new Player_();
+	camera.spawnEffect();
+}
+
 let camera = {
 	x: {
-		absolute: Math.round(map.width / 2),
+		absolute: Math.round(player.x + player.size - canvas.width / 2),
 		shift: 0,
 		get relative() {
 			return camera.x.absolute + camera.x.shift;
 		}
 	},
 	y: {
-		absolute: Math.round(map.height / 2),
+		absolute: Math.round(player.y + player.size - canvas.height / 2),
 		shift: 0,
 		get relative() {
 			return camera.y.absolute + camera.y.shift;
@@ -126,75 +231,33 @@ let camera = {
 	update: setInterval(function() {
 		if (keys.pressed.includes(87)) {
 			player.move(0, -1);
-			//camera.move(0, -1);
 		}
 		if (keys.pressed.includes(68)) {
 			player.move(1, 0);
-			//camera.move(1, 0);
 		}
 		if (keys.pressed.includes(83)) {
 			player.move(0, 1);
-			//camera.move(0, 1);
 		}
 		if (keys.pressed.includes(65)) {
 			player.move(-1, 0);
-			//camera.move(-1, 0);
 		}
-	}, 2),
+		if (keys.pressed.includes(38)) {
+			player.shoot(0);
+		}
+		if (keys.pressed.includes(39)) {
+			player.shoot(1);
+		}
+		if (keys.pressed.includes(40)) {
+			player.shoot(2);
+		}
+		if (keys.pressed.includes(37)) {
+			player.shoot(3);
+		}
+	}, 0.001),
 	get pos() {
 		return [camera.x.absolute, camera.y.absolute];
 	}
 };
-
-class Player_ {
-	constructor() {
-		this.size = 50;
-		this.hp = 5;
-		this.x = Math.round(map.width / 2 - this.size / 2);
-		this.y = Math.round(map.height / 2 - this.size / 2);
-		this.dots = [];
-		this.color = 'white';
-	}
-	get pos() {
-		return [this.x, this.y];
-	}
-	updateDots() {
-		this.dots = [];
-		for (let y = this.y; y <= this.y + this.size; y++) {
-			for (let x = this.x; x <= this.x + this.size; x++) {
-				this.dots.push(`${x}:${y}`);
-			}
-		}
-	}
-	move(x, y) {
-		this.x += x;
-		this.y += y;
-		camera.x.absolute += x;
-		camera.y.absolute += y;
-		this.updateDots();
-	}
-	harm() {
-		this.hp--;
-		camera.shake(5, 50, 1000);
-		if (this.hp <= 0) {
-			player.kill();
-		}
-	}
-	kill() {
-		this.gameOver = true;
-		camera.deathEffect();
-	}
-	respawn() {
-		respawnPlayer();
-		camera.respawnEffect();
-	}
-}
-let player = new Player_();
-
-function respawnPlayer() {
-	player = new Player_();
-	camera.spawnEffect();
-}
 
 function collides(ent1, ent2) {
 	for (let i of ent1.dots) {
@@ -236,7 +299,17 @@ class Enemy {
 				break;
 			}
 		}
+		this.xBackup = this.x;
+		this.yBackup = this.y;
 		this.radius = this.hp * 6 + 20;
+	}
+	craetePosBackup() {
+		this.xBackup = this.x;
+		this.yBackup = this.y;
+	}
+	restorePosBackup() {
+		this.x = this.xBackup;
+		this.y = this.yBackup;
 	}
 	updateDots() {
 		this.dots = [];
@@ -257,13 +330,15 @@ class Enemy {
 		}
 	}
 	move() {
+		this.craetePosBackup();
 		let seekNextPosition;
+		const parent = this;
 		if (this.x > player.x) {
 			if (this.y < player.y) {
 				seekNextPosition = function() {
-					for (let x = this.x - 1; x >= player.x; x--) {
-						for (let y = this.y; y <= player.y; y++) {
-							if ((y - this.y) / (player.y - this.y) == (x - this.x) / (player.x - this.x)) {
+					for (let x = parent.x - 1; x >= player.x; x--) {
+						for (let y = parent.y; y <= player.y; y++) {
+							if ((y - parent.y) / (player.y - parent.y) == (x - parent.x) / (player.x - parent.x)) {
 								return {x: x, y: y};
 							}
 						}
@@ -272,9 +347,9 @@ class Enemy {
 			}
 			if (this.y > player.y) {
 				seekNextPosition = function() {
-					for (let x = this.x - 1; x >= player.x; x--) {
-						for (let y = this.y; y >= player.y; y--) {
-							if ((y - this.y) / (player.y - this.y) == (x - this.x) / (player.x - this.x)) {
+					for (let x = parent.x - 1; x >= player.x; x--) {
+						for (let y = parent.y; y >= player.y; y--) {
+							if ((y - parent.y) / (player.y - parent.y) == (x - parent.x) / (player.x - parent.x)) {
 								return {x: x, y: y};
 							}
 						}
@@ -283,16 +358,16 @@ class Enemy {
 			}
 			if (this.y == player.y) {
 				seekNextPosition = function() {
-					return {x: this.x - 1, y: this.y};
+					return {x: parent.x - 1, y: parent.y};
 				}
 			}
 		}
 		if (this.x < player.x) {
 			if (this.y < player.y) {
 				seekNextPosition = function() {
-					for (let x = this.x + 1; x <= player.x; x++) {
-						for (let y = this.y; y <= player.y; y++) {
-							if ((y - this.y) / (player.y - this.y) == (x - this.x) / (player.x - this.x)) {
+					for (let x = parent.x + 1; x <= player.x; x++) {
+						for (let y = parent.y; y <= player.y; y++) {
+							if ((y - parent.y) / (player.y - parent.y) == (x - parent.x) / (player.x - parent.x)) {
 								return {x: x, y: y};
 							}
 						}
@@ -301,9 +376,9 @@ class Enemy {
 			}
 			if (this.y > player.y) {
 				seekNextPosition = function() {
-					for (let x = this.x + 1; x <= player.x; x++) {
-						for (let y = this.y; y >= player.y; y--) {
-							if ((y - this.y) / (player.y - this.y) == (x - this.x) / (player.x - this.x)) {
+					for (let x = parent.x + 1; x <= player.x; x++) {
+						for (let y = parent.y; y >= player.y; y--) {
+							if ((y - parent.y) / (player.y - parent.y) == (x - parent.x) / (player.x - parent.x)) {
 								return {x: x, y: y};
 							}
 						}
@@ -312,23 +387,42 @@ class Enemy {
 			}
 			if (this.y == player.y) {
 				seekNextPosition = function() {
-					return {x: this.x + 1, y: this.y};
+					return {x: parent.x + 1, y: parent.y};
 				}
 			}
 		}
 		if (this.x == player.x) {
 			if (this.y > player.y) {
 				seekNextPosition = function() {
-					return {x: this.x, y: this.y - 1};
+					return {x: parent.x, y: parent.y - 1};
 				}
 			}
 			if (this.y < player.y) {
 				seekNextPosition = function() {
-					return {x: this.x, y: this.y + 1};
+					return {x: parent.x, y: parent.y + 1};
 				}
 			}
 		}
-
+		try {
+			this.x = seekNextPosition().x;
+			this.y = seekNextPosition().y;
+		} catch {}
+		let checkAllCollisions = function(obj) {
+			for (let i of enemies) {
+				if (collides(obj, i)) {
+					return false;
+				}
+			}
+			return true;
+		}
+		if (!checkAllCollisions(this)) {
+			this.restorePosBackup();
+		}
+		if (collides(this, player)) {
+			player.harm();
+			this.kill(false);
+		}
+		this.updateDots();
 	}
 	harm() {
 		this.hp--;
@@ -339,14 +433,14 @@ class Enemy {
 	}
 	kill(spawnBullets) {
 		if (spawnBullets) {
-			enemyBullets.push(new Bullet(0));
-			enemyBullets.push(new Bullet(1));
-			enemyBullets.push(new Bullet(2));
-			enemyBullets.push(new Bullet(3));
-			enemyBullets.push(new Bullet(4));
-			enemyBullets.push(new Bullet(5));
-			enemyBullets.push(new Bullet(6));
-			enemyBullets.push(new Bullet(7));
+			enemyBullets.push(new Bullet(0, this));
+			enemyBullets.push(new Bullet(1, this));
+			enemyBullets.push(new Bullet(2, this));
+			enemyBullets.push(new Bullet(3, this));
+			enemyBullets.push(new Bullet(4, this));
+			enemyBullets.push(new Bullet(5, this));
+			enemyBullets.push(new Bullet(6, this));
+			enemyBullets.push(new Bullet(7, this));
 		}
 		this.phantom = true;
 		this.x = map.width;
@@ -354,17 +448,84 @@ class Enemy {
 	}
 }
 let enemies = [];
-function smartEnemySpawn() {
-	let enemySpawned = false;
-	for (let i of enemies) {
-		if (i.phantom) {
-			i = new Enemy(maxEnemyHp);
-			enemySpawned = true;
-			break;
+class EnemyBullet {
+	constructor(direction, parent) {
+		this.x = parent.x;
+		this.y = parent.y;
+		this.dots = [];
+		this.move = function() {};
+		switch (direction) {
+			case 0:
+				this.move = function() {
+					this.y--;
+				};
+				break;
+			case 1:
+				this.move = function() {
+					this.x++;
+					this.y--;
+				};
+				break;
+			case 2:
+				this.move = function() {
+					this.x++;
+				};
+				break;
+			case 3:
+				this.move = function() {
+					this.x++;
+					this.y++;
+				};
+				break;
+			case 4:
+				this.move = function() {
+					this.y++;
+				};
+				break;
+			case 5:
+				this.move = function() {
+					this.x--;
+					this.y++;
+				};
+				break;
+			case 6:
+				this.move = function() {
+					this.x--;
+				};
+				break;
+			case 7:
+				this.move = function() {
+					this.x--;
+					this.y--;
+				};
+				break;
 		}
 	}
-	if (!enemySpawned) {
-		enemies.push(new Enemy(maxEnemyHp));
+	updateDots() {
+		this.dots = [];
+		for (let y = this.y; y <= this.y + this.diameter; y++) {
+			for (let x = this.x; x <= this.x + this.diameter; x++) {
+				if ((x - this.x + this.radius) ** 2 + (y - this.y + this.radius) ** 2 <= this.radius ** 2) {
+					this.dots.push(`${x}:${y}`);
+				}
+			}
+		}
+	}
+}
+let enemyBullets = [];
+function smartEnemySpawn() {
+	if (enemies.length < enemiesLimit) {
+		let enemySpawned = false;
+		for (let i of enemies) {
+			if (i.phantom) {
+				i = new Enemy(maxEnemyHp);
+				enemySpawned = true;
+				break;
+			}
+		}
+		if (!enemySpawned) {
+			enemies.push(new Enemy(maxEnemyHp));
+		}
 	}
 }
 setInterval(function() {
@@ -378,7 +539,20 @@ setInterval(function() {
 			i.move();
 		}
 	}
-}, 10);
+}, 20);
+setInterval(function() {
+	if (player.shootCooldown > 0) {
+		player.shootCooldown--;
+	}
+}, 1);
+setInterval(function() {
+	for (let i of playerBullets) {
+		i.move();
+	}
+	for (let i of enemyBullets) {
+		i.move();
+	}
+}, 0.01);
 smartEnemySpawn();
 
 document.addEventListener('keydown', function(event) {
